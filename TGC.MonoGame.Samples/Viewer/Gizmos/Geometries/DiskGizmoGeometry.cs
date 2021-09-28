ab3e17bd2cde826599c6a6bbe0ba931cdc9baf76
avinitzca <avinitzca@gmail.com>
2021-09-28 01:36:20 -0300

    -Changed the Axis Lines 3D model. It now has correct normals.
    -Improved the way Axis Lines are drawn, now they don't require a Projection matrix.
    -Fixed a normal calculation in the Disk Gizmo, needs iteration.

--- TGC.MonoGame.Samples/Viewer/Gizmos/Geometries/DiskGizmoGeometry.cs
+++ TGC.MonoGame.Samples/Viewer/Gizmos/Geometries/DiskGizmoGeometry.cs
@@ -28,7 +28,7 @@ namespace TGC.MonoGame.Samples.Viewer.Gizmos.Geometries
             Array.Copy(originalIndices, 0, indices, 0, subdivisionsTimesTwo);
 
             Array.Copy(positions
-                .Select(position => new VertexPosition(new Vector3(position.Y, 0f, position.X)))
+                .Select(position => new VertexPosition(new Vector3(position.Y, position.X, 0f)))
                 .ToArray(), 0, vertices, 0, subdivisions);
             
             InitializeVertices(vertices);
@@ -46,16 +46,38 @@ namespace TGC.MonoGame.Samples.Viewer.Gizmos.Geometries
         public static Matrix CalculateWorld(Vector3 origin, Vector3 normal, float radius)
         {
             var rotation = Matrix.Identity;
-            if (!normal.Equals(Vector3.Up))
+            if (!normal.Equals(Vector3.Backward))
             {
                 // Rotate our disk!
                 // Pretty sure this can be optimized
-                var axis = Vector3.Cross(Vector3.Up, normal);
-                float amplitude = Vector3.Dot(Vector3.Up, normal);
-                rotation = Matrix.CreateFromQuaternion(Quaternion.CreateFromAxisAngle(axis, amplitude));
+                
+                //rotation.Up = normal;
+                var newLeft = Vector3.Cross(Vector3.Up, normal);
+                var newUp = Vector3.Cross(normal, newLeft);
+
+                rotation = Matrix.CreateLookAt(origin, origin + normal, newLeft);
+                //rotation = Matrix.CreateFromAxisAngle(new Vector3(-0.1169998f, 0.4435054f, 0.4435054f), 0.7700111f);
+
+                Matrix m = Matrix.Identity;
+                m.M31 = -normal.X;
+                m.M32 = -normal.Y;
+                m.M33 = -normal.Z;
+
+                var Right = Vector3.Cross(Vector3.Up, -normal);
+                var right = Right / MathF.Sqrt(MathF.Max(0.00001f, Vector3.Dot(Right, Right)));
+                m.M11 = right.X;
+                m.M12 = right.Y;
+                m.M13 = right.Z;
+
+                var c = Vector3.Cross(-normal, right);
+                m.M21 = c.X;
+                m.M22 = c.Y;
+                m.M23 = c.Z;
+
+                rotation = m;
             }
 
-            return Matrix.CreateScale(radius) * rotation * Matrix.CreateTranslation(origin);
+            return Matrix.CreateScale(radius) * rotation;
         }
     }
 }
